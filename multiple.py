ed02610b435a28bcb6f77e81dd5403fe8b86c842
Abhishek Dasari <abhishekdasari111@gmail.com>
2022-07-13 12:48:34 +0530

    added multiplication of 2 numbers

--- multiple.py
+++ multiple.py
@@ -1,4 +1,7 @@
-P=eval(input("Enter the Principal amount: "))
+'''P=eval(input("Enter the Principal amount: "))
 R=eval(input("Enter the Rate of Interest: "))
 Y=eval(input("Enter the Number of Years: "))
-print("The Simple Interest is: ",(P*R*Y)/100)
+print("The Simple Interest is: ",(P*R*Y)/100)'''
+a = 2
+b = 3
+print("Product of 2 num is ",a*b)
