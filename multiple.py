8cc8b61c1ad5194835cc6f042844c2ef9e70ac1c
Abhishek Dasari <abhishekdasari111@gmail.com>
2022-07-13 12:13:02 +0530

    multiplication of number

---
+++ multiple.py
@@ -0,0 +1,4 @@
+P=eval(input("Enter the Principal amount: "))
+R=eval(input("Enter the Rate of Interest: "))
+Y=eval(input("Enter the Number of Years: "))
+print("The Simple Interest is: ",(P*R*Y)/100)
